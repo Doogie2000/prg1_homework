3eb2def8770278138d7a3ef6d48ecccff12eacc6
Doogie2000 <43152190+Doogie2000@users.noreply.github.com>
2018-09-12 11:46:59 -0500

    Update and rename README.md to hw1.py

---
+++ hw1.py
@@ -0,0 +1,24 @@
+'''
+Problem 1
+'''
+
+print(151, 1.51, "151" , True )
+h="Hello "
+w="World!"
+hw= h+w
+print(hw)
+wateriswet=True
+earthisflat=False
+insanity=wateriswet and earthisflat
+print(insanity)
+
+'''
+Problem 2
+'''
+x=float("18")
+print(x)
+
+A1= ( ( 3 ** ( 1/2 ) ) /4 ) * ( x ** 2 )
+A2= x**2
+A3= (5 * ( ( 5+2 ) * (5*(1/2)))/4)*(x**2)
+print( A1,A2,A3 )
