7e5731273a08a2c416c3a931d4b167293230110e
Doogie Kirkhofer <doogiekirkhofer@Doogies-MacBook-Air.local>
2018-10-02 09:34:19 -0500

    hw3.py in progress

---
+++ hw3.py
@@ -0,0 +1,17 @@
+print('''
+Welcome to Saint John's Northwestern Military Academy!
+This is your Academic Planner Program that will help you keep your assignments in line
+''')
+
+homework_to_do=[]
+homework_due_dates=[]
+
+tests_quizzes=[]
+tests_quiz_dates=[]
+
+while(True):
+    '''
+    problem 1)
+    '''
+    input("?")
+hello friend
