a65d7a5764de5cd667168120f49de85aea7e9e6e
Doogie Kirkhofer <doogiekirkhofer@Doogies-MacBook-Air.local>
2018-10-16 09:34:33 -0500

    Partially Completed

--- largestneighbor_quiz.py
+++ largestneighbor_quiz.py
@@ -1,9 +1,16 @@
 raw_distance = input('''Please input your distances, separated by a space
 >>>''')
-print('''This is your distance
+print('''These are your distances
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ''' + raw_distance)
 cooked_distance = raw_distance.split(" ")
-og_distance = 
+
+og_distance = 0
 for distance in cooked_distance:
-    if int(distance) > og_distance
+    og_distance = int(distance) - og_distance
+    if int(distance) > og_distance :
+        greatest_distance = int(distance)
+
+
+print (greatest_distance)
+#not the solution, but as close as I can get within time    
