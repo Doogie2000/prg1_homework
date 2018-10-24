c70221a1bc4fe480c58d346ab250ea3b8c5f3709
Doogie Kirkhofer <doogiekirkhofer@Doogies-MacBook-Air.local>
2018-10-24 09:15:16 -0500

    Homework completed

--- hw5.py
+++ hw5.py
@@ -23,6 +23,7 @@ while True:
         print ("A good choice")
         time.sleep(1)
         break
+y_n = 0
 players={}
 players ["player_1"] = input("Player 1, what is your name? > ")
 players ["player_2"] = input("Player 2, what is your name? > ")
@@ -73,15 +74,16 @@ elif scores[players["player_1"]] == scores[players["player_2"]] :
     print("It is a tie!")
 else : 
     print("404 - data not found")
+for player in players.values() :
+    print (player + ", would you like to play to best of " + str(round_score +2))
 
-print ("Would you like to play again?")
+    time.sleep(1)
 
-time.sleep(1)
+    y_n += int(input("select yes (1) or no (2) > "))
 
-y_n = int(input("select yes (1) or no (2) > "))
-if y_n == 1 :
-    print ("A good choice.")
 if y_n == 2 :
+    print ("A good choice.")
+if y_n != 2 :
     print ('''
 
 
@@ -90,7 +92,7 @@ if y_n == 2 :
 
 
     ''')
-while y_n == 1 :
+while y_n == 2 :
     while (round_score < (desired_rounds + 2)) :
         print ('''
         Round #''' + str(round_score + 1) + '''
@@ -107,30 +109,41 @@ while y_n == 1 :
             print("your roll was " + str(score))
             scores[player] += int(score)
             time.sleep(1)
+            if player_score[players["player_1"]] > player_score[players["player_2"]] :
+                print(players["player_1"] + " is the round winner with " + str(player_score[players["player_1"]]) + " points!")
+            elif player_score[players["player_2"]] > player_score[players["player_1"]] :
+                print(players["player_2"] + " is the round winner with " + str(player_score[players["player_2"]]) + " points!")
+            elif player_score[players["player_1"]] == player_score[players["player_2"]] :
+                print("It is a tie!")
+            else : 
+                print("404 - data not found")
         round_score += 1
     time.sleep(1)
     if scores[players["player_1"]] > scores[players["player_2"]] :
-        print("Player 1 is the winner!")
+        print(players["player_1"] + " is the game winner with " + str(scores[players["player_1"]]) + " points!")
     elif scores[players["player_2"]] > scores[players["player_1"]] :
-        print("Player 2 is the winner!")
+        print(players["player_2"] + " is the game winner with " + str(scores[players["player_1"]]) + " points!")
     elif scores[players["player_1"]] == scores[players["player_2"]] :
         print("It is a tie!")
     else : 
         print("404 - data not found")
+    for player in players.values() :
+        print (player + ", would you like to play again?")
 
-    print ("Would you like to play again?")
-    time.sleep(1)
-    y_n = int(input("select yes (1) or no (2) > "))
-    if y_n == 1 :
-        print ("A good choice.")
+        time.sleep(1)
+
+        y_n += int(input("select yes (1) or no (2) > "))
+
     if y_n == 2 :
-        print ('''
+        print ("A good choice.")
+    if y_n != 2 :
+            print ('''
 
-        A strange game
+            A strange game
 
 
-        the only way to win is not to play
-        ''')
+            the only way to win is not to play
+            ''')
 
 
 
