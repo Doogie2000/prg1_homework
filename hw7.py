0a21acde161bd617a3248e9909b35f6c37b6b249
Doogie Kirkhofer <doogiekirkhofer@Doogies-Air.lan>
2018-11-15 09:22:14 -0600

    Completed, New Solution

--- hw7.py
+++ hw7.py
@@ -1,4 +1,5 @@
-def decode_ellen_speak(string, count):
+#Old Problem
+''' def decode_ellen_speak(string, count):
     string = list(string)
     if count > len(string) :
         return string
@@ -6,9 +7,16 @@ def decode_ellen_speak(string, count):
         string.remove(string[count-1])
         return decode_ellen_speak(string, count)
     else :
-        return decode_ellen_speak(string, count + 1)
+        return decode_ellen_speak(string, count + 1) '''
+#New Function
+def whale_speak(message):
+    if len(message) == 1 :
+        return message
+    elif message[0] != message[1] :
+        return message[0] + whale_speak(message[1:])
+    else:
+        return whale_speak(message[1:])
 
-
-print(decode_ellen_speak("hhhhhoooowwww areeeee yyyooouuuuu", 0))
+print(whale_speak("hhhhhoooowwww areeeee yyyooouuuuu"))
         
     
