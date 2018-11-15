7afbbefdcee269fcba8a4bd33365dac3a6b56d9c
Doogie Kirkhofer <doogiekirkhofer@Doogies-MacBook-Air.local>
2018-11-15 07:32:43 -0600

    Completed ish

--- hw7.py
+++ hw7.py
@@ -1,2 +1,14 @@
-def whale_then (string):
-    pass
+def decode_ellen_speak(string, count):
+    string = list(string)
+    if count > len(string) :
+        return string
+    elif count < len(string) and string[count] == string[count-1] and count > 0:
+        string.remove(string[count-1])
+        return decode_ellen_speak(string, count)
+    else :
+        return decode_ellen_speak(string, count + 1)
+
+
+print(decode_ellen_speak("hhhhhoooowwww areeeee yyyooouuuuu", 0))
+        
+    
